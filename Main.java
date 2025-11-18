747a91e63cce7ab5910dbaff7f4f6bf48ada8036
Aaniyah <aaniyah.ellis2027@gbwl.org>
2025-11-18 16:06:16 +0000

    2.8 notes

--- Main.java
+++ Main.java
@@ -18,6 +18,28 @@ public class Main {
          count ++;
       }
 
+      //FOR LOOPS: when you know HOW MANY TIMES to iterate
+      // for (initislize; test condition; change)
+      for (int i = 5; i <= 27; i++){
+         System.out.println(i);
+      }
+      // Modify the CHANGE step to skip certain number
+      for (int i = 0; i<= 10; i+=2){
+         System.out.println(i);
+      }
+      //Watch your INITIAL values and your OPERATOR
+      for (int i=0; i < 11; i += 2){
+         System.out.println(i);
+      }// this loop did the SAME THING as the one above
+
+      for (int i = 10; i > 0; i--){
+         System.out.println(i);
+         // Print a special message when i is 1
+         if (i == 1){
+            System.out.println("Blast off!!");
+         }
+      }
+
 
 
    } // END: Main method
